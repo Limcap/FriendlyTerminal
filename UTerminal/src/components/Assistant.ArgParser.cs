d888cae94c9e6acdad7798da310370f7b78aa4ee
Leandro <l.lino@columbia.edu>
2021-08-25 14:13:58 -0300

    @1 assistant has been decomposed in Assistant, CmdParser and ArgsParser.

---
+++ UTerminal/src/components/Assistant.ArgParser.cs
@@ -0,0 +1,295 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace Limcap.UTerminal {
+	public partial class Assistant {
+
+		[DebuggerDisplay( "{Length, nq} Arguments" )]
+		public unsafe class ArgParser {
+
+			public readonly List<Arg> list;
+			public readonly List<ACommand.Parameter> possible;
+			public bool IsLastParamInvalid { get; private set; }
+			//public readonly StringBuilder prediction;
+			//public static readonly ACommand.Parameter InvalidParam = new ACommand.Parameter("invalid",ACommand.Parameter.Type.ANY,false, "Invalid parameter" );
+
+
+
+
+
+
+
+
+			public ArgParser( int len ) {
+				list = new List<Arg>( len );
+				possible = new List<ACommand.Parameter>( len );
+				//prediction = new StringBuilder();
+			}
+
+
+
+
+
+
+
+
+			public Arg this[int i] {
+				get => list[i];
+				set => list[i] = value;
+			}
+
+
+
+
+
+
+
+
+			[DebuggerBrowsable( DebuggerBrowsableState.RootHidden )]
+			public Arg[] Elements => list.ToArray();
+			public int Length => list.Count;
+			public bool IsNull => list == null;
+			public Arg Last => IsNull ? new Arg( null ) : this[Length - 1];
+			//public ref Arg Last1 => ref this[Length - 1];
+			//public static Arg Null => new Arg( null );
+
+
+
+
+
+
+
+
+			public void Parse( PString input, ACommand.Parameter[] parameters ) {
+				if (parameters?.IsNullOrEmpty() ?? true || input.IsNull) return;
+				Expand( input );
+				MatchParameters( parameters );
+				CollectPossibilities( parameters );
+			}
+
+
+
+
+
+
+
+
+			public void Expand( PString input ) {
+				list.Clear();
+				var slicer = input.GetSlicer( ARGS_SEPARATOR );
+				while (slicer.HasNext) {
+					var slice = slicer.Next();
+					list.Add( new Arg( ref slice ) );
+				}
+			}
+
+
+
+
+
+
+
+
+			//public void Match( ACommand cmd ) { //List<ACommand.Parameter> outPredictionList, StringBuilder outPredictionString
+
+			//	possible.Clear();
+			//	prediction.Clear();
+
+			//	// If user has not typed in any args, mark all of them.
+			//	if (list.Count == 0) {
+			//		possible.AddRange( cmd.Parameters );
+			//	}
+
+			//	// Else check each parameters already typed in
+			//	else {
+
+			//		// If the last parameter has an incomplete name:
+			//		var arg = list[list.Count - 1];
+			//		if (!arg.NameIsComplete) {
+
+			//			// Add the possible parameters to the aux list
+			//			int index = 0;
+			//			while ((index = cmd.Parameters.GetIndexByNamePrefix( arg.name, index )) > -1) {
+			//				//arg.parameter = cmd.Parameters[index];
+			//				possible.Add( cmd.Parameters[index] );
+			//				index++;
+			//			}
+
+			//			// If there's no possible parameters, say 'invalid parameters'
+			//			if (possible.Count == 0) {
+			//				prediction.Append( "Invalid parameter" );
+			//			}
+
+			//			// Else if there are:
+			//			else {
+
+			//				// Verify previous parametes with complete and if they exist within the aux list, remove them.
+			//				for (int i = 0; i < list.Count - 1; i++) {
+			//					arg = list[i];
+			//					if (arg.NameIsComplete) {
+			//						index = possible.GetIndexByName( arg.name );
+			//						if (index > -1) possible.RemoveAt( index );
+			//					}
+			//				}
+			//			}
+
+
+			//		}
+
+			//		// If the name of the last parameter is complete, check if it is valid or not.
+			//		else {
+			//			if (cmd.Parameters.GetIndexByName( arg.name ) == -1)
+			//				prediction.Append( "Invalid parameter" );
+			//			else prediction.Append( "," );
+			//		}
+			//	}
+
+			//	// Last, build the string
+			//	foreach (var p in possible)
+			//		prediction.Append( p.optional ? $"[{p.name}=]" : $"{p.name}=" ).Append( PREDICTIONS_SEPARATOR );
+			//}
+
+
+
+
+
+
+
+
+			public void MatchParameters( ACommand.Parameter[] parameters ) {
+				for (int i = 0; i < Length; i++) {
+					var arg = list[i];
+					if (arg.NameIsComplete) {
+						var paramIndex = parameters.GetIndexByName( this[i].name );
+						var p = paramIndex > -1 ? parameters[paramIndex] : null;
+						if (p != null) {
+							arg.parameter = p;
+							list[i] = arg;
+						}
+					}
+				}
+			}
+
+
+
+
+
+
+
+
+			public void CollectPossibilities( ACommand.Parameter[] parameters ) {
+				possible.Clear();
+
+				if (list.Count == 0) return;
+				var arg = list[list.Count - 1];
+				int index = 0;
+				if (!arg.NameIsComplete) {
+					while ((index = parameters.GetIndexByNamePrefix( arg.name, index )) > -1) {
+						var p = parameters[index];
+						if (!IsAlreadyMatched( p )) possible.Add( p );
+						index++;
+					}
+					IsLastParamInvalid = possible.Count == 0;
+				}
+				else IsLastParamInvalid = false;
+			}
+
+
+
+
+
+
+
+
+			public bool IsAlreadyMatched( ACommand.Parameter p ) {
+				for (int i = 0; i < list.Count - 1; i++)
+					if (list[i].NameIsComplete && list[i].parameter == p) return true;
+				return false;
+			}
+
+
+
+
+
+
+
+
+			public void GetPredictionPossibilities( StringBuilder res_text ) {
+				res_text.Clear();
+				// Last, build the string
+				//if( possible.Count == 1 && possible[0] == InvalidParam )
+				//text.Append( possible[0].description );
+				if (IsLastParamInvalid)
+					res_text.Append( "Invalid parameter" );
+				else if (possible.Count == 0)
+					res_text.Append( ',' );
+				else foreach (var p in possible)
+						res_text.Append( p.optional ? $"[{p.name}=]" : $"{p.name}=" ).Append( PREDICTIONS_SEPARATOR );
+			}
+
+
+
+
+
+
+
+
+			//public void ForgePredictedText( int index, StringBuilder res_text ) {
+			//	for (int i = 0; i < Length; i++) {
+			//		var a = this[i];
+			//		if (Length > 1 && i < Length - 1) {
+			//			res_text.Append( a.name ).Append( ARG_VALUE_SEPARATOR ).Append( a.value )
+			//				.Append( ARGS_SEPARATOR ).Append( CmdParser.CMD_WORD_SEPARATOR );
+			//		}
+			//		else {
+			//			if (a.NameIsComplete) {
+			//				res_text.Append( a.name ).Append( ARG_VALUE_SEPARATOR ).Append( a.value );
+			//				if (possible.Count == 0) res_text.Append( ARGS_SEPARATOR );
+			//			}
+			//			// Append the predicted parameter
+			//			else if (index != -1)
+			//				res_text.Append( possible?[index].name ).Append( ARG_VALUE_SEPARATOR );
+			//		}
+			//	}
+			//}
+
+
+
+
+
+
+
+			public void GetConfirmedText( StringBuilder res_text ) {
+				for (int i = 0; i < Length; i++) {
+					var a = this[i];
+					if (Length > 1 && i < Length - 1) {
+						res_text.Append( a.name ).Append( ARG_VALUE_SEPARATOR ).Append( a.value )
+							.Append( ARGS_SEPARATOR ).Append( CmdParser.CMD_WORD_SEPARATOR );
+					}
+					else {
+						if (a.NameIsComplete) {
+							res_text.Append( a.name ).Append( ARG_VALUE_SEPARATOR ).Append( a.value );
+							if (possible.Count == 0) res_text.Append( ARGS_SEPARATOR );
+						}
+					}
+				}
+			}
+
+
+
+
+
+
+
+
+			public void GetPredictionText( int index, StringBuilder res_text ) {
+				if (index == -1) return;
+				else res_text.Append( possible?[index].name ).Append( ARG_VALUE_SEPARATOR );
+			}
+		}
+	}
+}
---
+++ UTerminal/src/components/Assistant.CmdParser.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Limcap.UTerminal {
+	public partial class Assistant {
+
+		public class CmdParser {
+
+			public const char CMD_WORD_SEPARATOR = ' ';
+			public const char CMD_TERMINATOR = ':';
+			public const string CMD_TERMINATOR_AS_STRING = ":";
+
+			protected readonly Node _invalidNode;
+			protected readonly Node _startNode;
+
+			// Output variables. Will be used to store the results from parsing.
+			public Node confirmedNode;
+			public List<Node> predictedNodes;
+			public ACommand parsedCmd;
+
+
+
+
+
+
+
+			public CmdParser( Dictionary<string, Type> commandsSet, string locale ) {
+				_invalidNode = new Node() { word = "Invalid command" };
+				_startNode = new Node() { word = "@" };
+
+				// Results
+				confirmedNode = _startNode;
+				predictedNodes = new List<Node>();
+
+				Stopwatch sw = new Stopwatch();
+				sw.Start();
+				var memoryBefore = GC.GetTotalMemory( true );
+				Node.BuildTree( commandsSet, CMD_WORD_SEPARATOR, CMD_TERMINATOR_AS_STRING, _startNode );
+				var memoryAfter = GC.GetTotalMemory( true );
+				var memoryOccupied = memoryAfter - memoryBefore;
+				sw.Stop();
+				Trace.WriteLine( "memory occupied by tree: " + memoryOccupied );
+				Trace.WriteLine( "time taken to build tree: " + sw.ElapsedMilliseconds );
+			}
+
+
+
+
+
+
+
+
+			public void Parse( PString inputString, string locale )
+				=>Parse( locale, inputString, _startNode, _invalidNode, ref confirmedNode, ref parsedCmd, ref predictedNodes );
+
+			//public void ConstructParsedCmd( string locale )
+			//	=>ConstructCommand( confirmedNode, locale, ref parsedCmd );
+
+			public void GetPredictionPossibilitiesText( StringBuilder res_string )
+				=>ForgePredictionPossibilitiesText( predictedNodes, res_string );
+
+			public void GetConfirmedText( StringBuilder res_string )
+				=> ForgeConfirmedText( _startNode, res_string );
+
+			public void GetPredictionText( int indexOfPrediction, StringBuilder res_string )
+				=> ForgePredictionText( predictedNodes, indexOfPrediction, res_string );
+
+
+
+
+
+
+
+
+			#region PURE FUNCTIONS
+			#endregion
+
+
+
+
+			public unsafe static void Parse(
+				string locale, PString input, Node startNode, Node invalidNode,
+				ref Node res_confirmed, ref ACommand res_cmd, ref List<Node> res_predicted
+				) {
+				res_predicted.Clear();
+				res_confirmed = startNode;
+				
+				// If the input is empty, the confirmed is the start node, and the predicted will be all reachable from it.
+				if (input.IsNullOrEmpty) {
+					res_predicted.AddRange( res_confirmed.edges );
+					return;
+				}
+
+				// Creates a stack-alloc copy of the input so it can be modified freely.
+				var ptr = stackalloc char[input.len];
+				for (int i = 0; i < input.len; i++) ptr[i] = input[i];
+				var inputTemp = new PString( ptr, input.len );
+
+				FixInputTermination( ref inputTemp );
+				
+				FindConfirmedWords( startNode, ref inputTemp, ref res_confirmed );
+
+				ConstructCommand( res_confirmed, locale, ref res_cmd );
+
+				if (res_cmd is null) {
+					// Ignores one whitespace at the end of the input. Since the end word of the invoke command does not end
+					// in separator, this space would render the input invalid. This is important because when we press tab
+					// to autocompete a word, the predictions wont change until we type space (or a solid char). So when use
+					// autocomplete on the last word and then press space, the status bar would show invalid command.
+					// Doing this, we allow the input to have this extra space between the end of the string and the terminator
+					// and the status bar will show, instead of 'invalid command', the prediction for the terminator char,
+					// even though the actual invoke string does not end in space.
+					if (inputTemp == ' ') inputTemp.len--;
+
+					PredictNextWord( res_confirmed, inputTemp, invalidNode, ref res_predicted );
+				}				
+			}
+
+
+
+
+
+
+
+
+			public static void FixInputTermination( ref PString ps ) {
+				if (ps.len > 1 && ps[ps.len - 1] == CMD_TERMINATOR) {
+					while (ps[ps.len - 2] == CMD_WORD_SEPARATOR) {
+						ps[ps.len - 2] = CMD_TERMINATOR;
+						ps[ps.len - 1] = CMD_WORD_SEPARATOR;
+						ps.len--;
+					}
+				}
+			}
+
+
+
+
+
+
+
+
+			public unsafe static void FindConfirmedWords( Node startNode, ref PString input, ref Node res_confirmed ) {
+				res_confirmed = startNode.Traverse( ref input );
+			}
+
+
+
+
+
+
+
+
+			public static void PredictNextWord( Node confirmedNode, PString inputResidue, Node invalidNode, ref List<Node> res_predicted ) {
+				// Select the possible next nodes.
+				res_predicted.AddRange( confirmedNode.edges.Where( n => n.word.StartsWith( inputResidue ) ).OrderBy( n => n.word ) );
+				if (res_predicted.Count == 0) res_predicted.Add( invalidNode );
+			}
+
+
+
+
+
+
+
+
+			public static void ConstructCommand( Node confirmedNode, string locale, ref ACommand res_cmd ) {
+				if (confirmedNode.cmdType == null) {
+					res_cmd = null;
+				}
+				else if (res_cmd == null || res_cmd.GetType() != confirmedNode.cmdType)
+					res_cmd = confirmedNode.cmdType.IsSubclassOf( typeof( ACommand ) )
+						? (ACommand)Activator.CreateInstance( confirmedNode.cmdType, locale )
+						: null;
+			}
+
+
+
+
+
+
+
+
+			public static void ForgePredictionPossibilitiesText( List<Node> possibleNextNodes, StringBuilder result ) {
+				if (possibleNextNodes.IsNullOrEmpty()) result.Reset( "Command not found" );
+				//Index = -1;
+				result.Reset();
+				foreach (var n in possibleNextNodes) result.Append( n.word ).Append( PREDICTIONS_SEPARATOR );
+			}
+
+
+
+
+
+
+
+
+			public static void ForgeConfirmedText( Node startNode, StringBuilder result ) {
+				var node = startNode;
+				while (node.next != null) {
+					result.Append( node.next.word );
+					node = node.next;
+					if (node.IsLeafNode) result.Append( ' ' );// .word == CMD_TERMINATOR_AS_STRING )
+				}
+			}
+
+
+
+
+
+
+
+			public static void ForgePredictionText( List<Node> predictedNodes, int predictionIndex, StringBuilder res_text ) {
+				if (predictionIndex == -1) return;
+				res_text.Append( ((PString)predictedNodes[predictionIndex].word).Trim() );
+			}
+		}
+	}
+}
