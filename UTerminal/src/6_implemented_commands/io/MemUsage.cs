ec3bf5d0cbf5cf64e0c95439e6931d66802af7cd
Leandro <l.lino@columbia.edu>
2021-08-25 23:49:48 -0300

    new command: mem usage

---
+++ UTerminal/src/6_implemented_commands/io/MemUsage.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace Limcap.UTerminal.Cmds.IO {
+	public class MemUsage : ACommand {
+
+		public MemUsage( string locale ) : base( locale ) { }
+
+
+		public const string DEFAULT_LOCALE = "enus";
+		public const string INVOKE_TEXT = "mem usage";
+
+
+
+		protected private override string DescriptionBuilder() {
+			return Txt( "desc" );
+		}
+		
+
+		protected private override Parameter[] ParametersBuilder() {
+			return null;
+		}
+
+
+		public override string MainFunction( Terminal t, Arg[] args ) {
+			System.GC.Collect();
+			var mem = GC.GetTotalMemory( true );
+			return "\nUsed memory: " + mem;
+		}
+	}
+}
