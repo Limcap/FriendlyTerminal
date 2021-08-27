4f284be1c72aec3add6a395e73a80196b561d1bd
Leandro <l.lino@columbia.edu>
2021-08-27 15:45:04 -0300

    Limcap's Friendly Terminal v0.3

--- FTerminal/Properties/AssemblyInfo.cs
+++ FTerminal/Properties/AssemblyInfo.cs
@@ -1,15 +1,16 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("TextboxTerminal")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle( "Lift" )]
+[assembly: AssemblyDescription( "Limcap's Friendly Terminal" )]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("TextboxTerminal")]
+[assembly: AssemblyCompany( "Limcap" )]
+[assembly: AssemblyProduct( "Friendly Terminal" )]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +33,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion( "0.3.0.0" )]
+[assembly: AssemblyFileVersion( "0.3.0.0" )]
+[assembly: NeutralResourcesLanguage( "en" )]
