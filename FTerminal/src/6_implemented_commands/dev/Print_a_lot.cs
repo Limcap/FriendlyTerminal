f91d7891059a101083f9244922ac2650a5019d8c
Leandro <l.lino@columbia.edu>
2021-08-31 14:43:55 -0300

    improv cmd print-a-lot

--- FTerminal/src/6_implemented_commands/dev/Print_a_lot.cs
+++ FTerminal/src/6_implemented_commands/dev/Print_a_lot.cs
@@ -6,39 +6,41 @@ using System.Text;
 namespace Limcap.FTerminal.Cmds.Dev {
 	public class Print_a_lot : ACommand {
 
-		//public static void Register( TerminalClient term ) {
-		//	term.RegisterCommand( "command2", MainFunction, HelpInfo );
-		//	Assembly assembly = Assembly.LoadFrom( "MyNice.dll" );
-		//	Type type = assembly.GetType( "MyType" );
-		//	ICommand instanceOfMyType = (ICommand) Activator.CreateInstance( type );
-		//}
-
-
 		public const string INVOKE_TEXT = "dev, print-a-lot";
+		public const int REQUIRED_PRIVILEGE = 0;
+		
 
 
-		public const int REQUIRED_PRIVILEGE = 2;
+		
+		protected override string DescriptionBuilder() => "Very long repetitive text.";
 
 
-		//public const string HELP_INFO =
-		//	"DESCRIPTION:\n" +
-		//	"\tVery long repetitive text.";
+
+
+		protected override Parameter[] ParametersBuilder() => new[] {
+			new Parameter("length", Parameter.Type.INTEGER, true, "Length of the text in characters.")
+		};
+
+
 
 
 		public Print_a_lot( string locale ) : base( locale ) {}
 
 
+
+
 		public override string MainFunction( Terminal t, Arg[] args ) {
+			int length = args.Get( Parameters[0] ).SafeParseInt() ?? 1000;
 			var sb = new StringBuilder();
-			for (int i = 0; i < 2000; i++)
-				sb.Append( "this is a hundred characters long string, with the solo purpose of testing long strings. the end...\n" );
+			for (int i = 0; i < length; i = i + loopText2.Length)
+				sb.Append( loopText2 );
 			return sb.ToString();
 		}
 
 
-		protected override string DescriptionBuilder() => "Very long repetitive text.";
 
 
-		protected override Parameter[] ParametersBuilder() => null;
+		public const string loopText1 = "this is a hundred characters long string, with the solo purpose of testing long strings. the end...\n";
+		public const string loopText2 = "the quick brown fox jumped over the lazy dog while chasing a shining dragonfly\n";
 	}
 }
