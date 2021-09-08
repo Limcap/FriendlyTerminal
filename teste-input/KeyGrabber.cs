c6a5f16494670dd834bb48e0944542de2b8f5a13
Leandro <l.lino@columbia.edu>
2021-09-08 07:40:18 -0300

    teste-input project

---
+++ teste-input/KeyGrabber.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WpfApp1 {
+	public class KeyGrabber {
+
+		static byte[] _keyboardState = new byte[256];
+		readonly static StringBuilder sb = new StringBuilder( 2 );
+
+
+		public enum MapType : uint {
+			MAPVK_VK_TO_VSC = 0x0,
+			MAPVK_VSC_TO_VK = 0x1,
+			MAPVK_VK_TO_CHAR = 0x2,
+			MAPVK_VSC_TO_VK_EX = 0x3,
+		}
+
+
+
+
+		[DllImport( "user32.dll" )]
+		public static extern int ToUnicode(
+			 uint wVirtKey,
+			 uint wScanCode,
+			 byte[] lpKeyState,
+			 [Out, MarshalAs(UnmanagedType.LPWStr, SizeParamIndex = 4)]
+				StringBuilder pwszBuff,
+			 int cchBuff,
+			 uint wFlags
+		);
+
+
+
+
+		[DllImport( "user32.dll" )]
+		public static extern bool GetKeyboardState( byte[] lpKeyState );
+
+
+
+
+		[DllImport( "user32.dll" )]
+		public static extern uint MapVirtualKey( uint uCode, MapType uMapType );
+
+
+
+
+		public static char GetCharFromKey( Key key ) {
+			char ch = (char)0;
+
+			int virtualKey = KeyInterop.VirtualKeyFromKey( key );
+			GetKeyboardState( _keyboardState );
+			//if (key == Key.A) File.WriteAllBytes( "../bytesA.bin", _keyboardState );
+			//if (key == Key.Oem7) File.WriteAllBytes( "../bytes'.bin", _keyboardState );			if (key == Key.A) File.WriteAllBytes( "../bytesA.bin", _keyboardState );
+			//if (key == Key.A) _keyboardState = File.ReadAllBytes( "../o-bytesA.bin" );
+			//if (key == Key.Oem7) _keyboardState = File.ReadAllBytes( "../o-bytes'.bin" );
+			uint scanCode = MapVirtualKey( (uint)virtualKey, MapType.MAPVK_VK_TO_VSC );
+			sb.Clear();
+
+			int result = ToUnicode( (uint)virtualKey, scanCode, _keyboardState, sb, sb.Capacity, 0 );
+			switch (result) {
+				case -1:
+					break;
+				case 0:
+					break;
+				case 1: {
+					ch = sb[0];
+					break;
+				}
+				default: {
+					ch = sb[0];
+					break;
+				}
+			}
+			return ch;
+		}
+	}
+}
---
+++ teste-input/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1 {
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window {
+		public MainWindow() {
+			InitializeComponent();
+			var margin = new Thickness( 1 );
+
+
+
+			var doc = new FlowDocument() { Background = Brushes.LightGray, PagePadding = margin,  };
+			var view = new FlowDocumentScrollViewer() { Focusable = false, Background = Brushes.LightGray, Height = 30, Padding = margin, IsToolBarVisible =false };
+			view.Document = doc;
+			doc.PreviewMouseDown += ( o, a ) => doc.Focus();
+			doc.GotFocus += ( o, a ) => doc.Background = Brushes.LightBlue;
+			doc.LostFocus += ( o, a ) => doc.Background = Brushes.LightGray;
+			doc.PreviewKeyDown += ( o, a ) => {
+				var key = KeyGrabber.GetCharFromKey( a.Key );
+				if ( key != 0 ) doc.ContentEnd.InsertTextInRun( key.ToString() );
+			};
+			doc.PreviewKeyUp += ( o, a ) => { var b = KeyGrabber.GetCharFromKey( a.Key ); };
+
+
+
+			var txt = new TextBlock() { Focusable = true, Background = Brushes.LightGray, Height = 30, IsEnabled = true, Padding = margin };
+			txt.PreviewMouseDown += ( o, a ) => txt.Focus();
+			txt.GotFocus += ( o, a ) => txt.Background = Brushes.LightBlue;
+			txt.LostFocus += ( o, a ) => txt.Background = Brushes.LightGray;
+			txt.PreviewKeyDown += ( o, a ) => {
+				var key = KeyGrabber.GetCharFromKey( a.Key );
+				if (key != 0) txt.ContentEnd.InsertTextInRun( key.ToString() );
+			};
+			txt.PreviewKeyUp += ( o, a ) => { var b = KeyGrabber.GetCharFromKey( a.Key ); };
+
+
+
+			stackPanel.Children.Add( view );
+			stackPanel.Children.Add( txt );
+		}
+	}
+}
