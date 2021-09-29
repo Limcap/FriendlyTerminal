79cb34d9718b204d48ef84e33dbfc6952d4a7fa8
Leandro <l.lino@columbia.edu>
2021-09-29 04:32:15 -0300

    BUILD 0.7.7.16

--- FTerminal/Properties/AssemblyInfo.cs
+++ FTerminal/Properties/AssemblyInfo.cs
@@ -33,6 +33,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion( "0.7.6.15" )]
-[assembly: AssemblyFileVersion( "0.7.6.15" )]
+[assembly: AssemblyVersion( "0.7.7.16" )]
+[assembly: AssemblyFileVersion( "0.7.7.16" )]
 [assembly: NeutralResourcesLanguage( "en" )]
